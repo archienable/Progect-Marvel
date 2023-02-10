f2115190e064c05cd980036a5e3e15759ac5cf14
Artem Kozhanov <artemkozhanov@MacBook-Air-Artem.local>
2023-02-10 16:49:29 +0300

    reactMarvel

--- src/Content/HomePage/HomePage.js
+++ src/Content/HomePage/HomePage.js
@@ -6,6 +6,20 @@ const HomePage = () => {
         <div>
             <div className='pictures' style={{backgroundImage: `url(https://cdn.marvel.com/content/1x/antmanandthewaspquantumania_lob_mas_dsk_03.jpg)`}}></div>
             HOME PAGE
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
         </div>
     );
 };
