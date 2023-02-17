214da1b9b69eec4da7d7f07f51226e7ba31081cc
Artem Kozhanov <artemkozhanov@MacBook-Air-Artem.local>
2023-02-17 18:03:20 +0300

    reactMarvel

--- src/Content/Movies/FilmInfo/FilmInfo.js
+++ src/Content/Movies/FilmInfo/FilmInfo.js
@@ -4,11 +4,7 @@ import {useParams} from "react-router-dom";
 import backLogo from "../../../picture/Marvel_Logo.png";
 import RelatedMovies from "../../RelatedMovies/RelatedMovies";
 import PacmanLoader from "react-spinners/PacmanLoader";
-import Favorites from "../../Favorites/Favorites";
 
-// function OnClick() {
-//     return null;
-// }
 
 const FilmInfo = () => {
 
@@ -30,19 +26,25 @@ const FilmInfo = () => {
         setLoading(false)
     }
 
-    let styleFavorite = `${style.favorite} `
+    // let styleFavorite = `${style.favorite} `
     const result = newData.find(film => {
         return film.id === data.id
     })
 
-    if (result) {
-        styleFavorite += style.favSuccess
-    }
+    // if (result) {
+    //     styleFavorite += style.svg
+    // }
 
     const favorites = () => {
-        const upData =  [...newData, data]
-        localStorage.setItem('Favorites', JSON.stringify(upData))
-        setNewData(upData)
+        if (result) {
+            const upData =  [...newData].filter(film => film.id !== data.id)
+            localStorage.setItem('Favorites', JSON.stringify(upData))
+            setNewData(upData)
+        } else {
+            const upData =  [...newData, data]
+            localStorage.setItem('Favorites', JSON.stringify(upData))
+            setNewData(upData)
+        }
     }
 
     const override = {
@@ -75,7 +77,15 @@ const FilmInfo = () => {
                 </div>
                 <div className={style.containerInfo}>
                     <div className={style.title}>Title: <span className={style.info}>{data.title}</span></div>
-                    <div className={styleFavorite} onClick={favorites}>Favorites</div>
+                    <div className={style.favorite} onClick={favorites}>
+                        <svg className={style.svg} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 100 100" width="50px" height="50px">
+                            <path d="M24.501,98.001c-1.021,0-2.042-0.245-2.952-0.709C19.361,96.179,18,93.959,18,91.5v-75	C18,10.701,22.701,6,28.5,6h47C81.299,6,86,10.701,86,16.5v75.501c0,3.314-2.686,6-5.999,6h0c-1.299,0-2.562-0.421-3.601-1.2L50,77	l3.011,1.949L28.322,96.757C27.215,97.567,25.889,98.001,24.501,98.001z" opacity=".35"/>
+                            <path fill="black" d="M22.501,96.001c-1.021,0-2.042-0.245-2.952-0.709C17.361,94.179,16,91.959,16,89.5v-75	C16,8.71,20.71,4,26.5,4h47C79.29,4,84,8.71,84,14.5v75c0,2.459-1.361,4.679-3.552,5.793C79.54,95.756,78.521,96,77.5,96	c-1.387,0-2.712-0.432-3.831-1.249L50,77.537l-23.678,17.22C25.215,95.567,23.889,96.001,22.501,96.001z"/>
+                            <path fill={result ? 'red' : "white"} d="M26.5,10.5h47c2.209,0,4,1.791,4,4v75L50,69.5l-27.5,20v-75C22.5,12.291,24.291,10.5,26.5,10.5z"/>
+                            <path fill="none" stroke="#40396e" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="3" d="M26.5,10.5h47	c2.209,0,4,1.791,4,4v75L50,69.5l-27.5,20v-75C22.5,12.291,24.291,10.5,26.5,10.5z"/>
+                        </svg>
+                        Favorites
+                    </div>
                     <div className={style.title}>Release date: <span className={style.info}>{data.release_date}</span> </div>
                     <div className={style.title}>Duration: <span className={style.info}>{data.duration} min</span> </div>
                     <div className={style.title}>Directed by: <span className={style.info}>{data.directed_by}</span></div>
--- src/Content/Movies/Movies.js
+++ src/Content/Movies/Movies.js
@@ -15,6 +15,7 @@ const Movies = () => {
     const navigate = useNavigate();
     let [searchParams] = useSearchParams();
     const [loading, setLoading] = useState(true)
+    const [newData, setNewData] = useState(JSON.parse(localStorage.getItem('Favorites')) || [])
 
     const currentPage = searchParams.get("page") || 1
 
@@ -26,11 +27,27 @@ const Movies = () => {
         .then(res => res.json())
         .then(data => {
             setData(data.data)
-            console.log(data.total)
             setTotalPages(data.total / LIMIT)
             setLoading(false)
         })
     },[currentPage])
+
+    // const result = newData.find(favoriteFilm => {
+    //     // console.log('ARTEM',data)
+    //     return favoriteFilm.id === oneDataFilm.id
+    // })
+    //
+    // const favorites = (selectedFilm) => {
+    //     if (result) {
+    //         const upData =  [...newData].filter(film => film.id !== selectedFilm.id)
+    //         localStorage.setItem('Favorites', JSON.stringify(upData))
+    //         setNewData(upData)
+    //     } else {
+    //         const upData =  [...newData, selectedFilm]
+    //         localStorage.setItem('Favorites', JSON.stringify(upData))
+    //         setNewData(upData)
+    //     }
+    // }
 
     const override = {
         display: "block",
@@ -55,6 +72,24 @@ const Movies = () => {
                 <h2>MARVEL MOVIES</h2>
                 <div className={style.contentMovies}>
                     {data.map( film => {
+
+                        const result = newData.find(favoriteFilm => {
+                            // console.log('ARTEM',data)
+                            return favoriteFilm.id === film.id
+                        })
+
+                        const favorites = (selectedFilm) => {
+                            if (result) {
+                                const upData =  [...newData].filter(film => film.id !== selectedFilm.id)
+                                localStorage.setItem('Favorites', JSON.stringify(upData))
+                                setNewData(upData)
+                            } else {
+                                const upData =  [...newData, selectedFilm]
+                                localStorage.setItem('Favorites', JSON.stringify(upData))
+                                setNewData(upData)
+                            }
+                        }
+
                         return (
                             <Link className={style.link} to={`/movies/${film.id}`}>
                                 <div className={style.film} >
@@ -63,8 +98,20 @@ const Movies = () => {
                                             <div className={style.img} style={{backgroundImage: `url(${film.cover_url})`}}></div>
                                         </div>
                                     </div>
-                                    <div>
+                                    <div className={style.infoFilm}>
                                         <div className={style.title}>{film.title}</div>
+                                        <div className={style.favorite} onClick={(event) => {
+                                            event.preventDefault()
+                                            event.stopPropagation()
+                                            favorites(film)
+                                        }}>
+                                            <svg className={style.svg} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 100 100" width="20px" height="20px">
+                                                <path d="M24.501,98.001c-1.021,0-2.042-0.245-2.952-0.709C19.361,96.179,18,93.959,18,91.5v-75	C18,10.701,22.701,6,28.5,6h47C81.299,6,86,10.701,86,16.5v75.501c0,3.314-2.686,6-5.999,6h0c-1.299,0-2.562-0.421-3.601-1.2L50,77	l3.011,1.949L28.322,96.757C27.215,97.567,25.889,98.001,24.501,98.001z" opacity=".35"/>
+                                                <path fill="black" d="M22.501,96.001c-1.021,0-2.042-0.245-2.952-0.709C17.361,94.179,16,91.959,16,89.5v-75	C16,8.71,20.71,4,26.5,4h47C79.29,4,84,8.71,84,14.5v75c0,2.459-1.361,4.679-3.552,5.793C79.54,95.756,78.521,96,77.5,96	c-1.387,0-2.712-0.432-3.831-1.249L50,77.537l-23.678,17.22C25.215,95.567,23.889,96.001,22.501,96.001z"/>
+                                                <path fill={result ? 'red' : "white"} d="M26.5,10.5h47c2.209,0,4,1.791,4,4v75L50,69.5l-27.5,20v-75C22.5,12.291,24.291,10.5,26.5,10.5z"/>
+                                                <path fill="none" stroke="#40396e" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="3" d="M26.5,10.5h47	c2.209,0,4,1.791,4,4v75L50,69.5l-27.5,20v-75C22.5,12.291,24.291,10.5,26.5,10.5z"/>
+                                            </svg>
+                                        </div>
                                         <div className={style.date}>{moment(film.release_date).format("D MMM YYYY")}</div>
                                     </div>
                                 </div>
